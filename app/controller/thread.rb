e90a1e582201f4e4bb3c8de875373f22dd89d9ef
Michael Trommer <mictro@gmail.com>
2010-11-19 01:43:09 +0100

    stuff

---
+++ app/controller/thread.rb
@@ -0,0 +1,32 @@
+#
+#
+# Author:  Michael 'entropie' Trommer <mictro@gmail.com>
+#
+
+class Phread < PalavrController
+  map "/s"
+  set_layout("layout") # => [:index]) {|path, wish| not request.xhr? }
+
+  helper :auth
+  before_all(){
+    login_required
+  }
+
+
+  # TODO: category images
+  def index(phread = nil)
+    @phread = Phread.get(phread)
+  end
+
+end
+
+
+
+=begin
+Local Variables:
+  mode:ruby
+  fill-column:70
+  indent-tabs-mode:nil
+  ruby-indent-level:2
+End:
+=end
